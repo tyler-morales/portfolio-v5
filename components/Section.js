027508b22761bc18a65235e809e992c3aa9770f8
Tyler Morales <44333523+tyler-morales@users.noreply.github.com>
2022-01-29 23:23:59 -0600

    🧰 Initial setup (#1)
    * 🗑️ Remove unused boiler plate Next files
    
    * 📦  Install and configure Tailwind
    Set custom fonts and colors
    
    * 🍱 Add icons
    Icons for skills section
    
    * 🍱 Add images
    Image are subject to change
    
    * 🏗️ Add section files
    Added a grid layout in index and section layout files

---
+++ components/Section.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Section({children}) {
+  return <section className="py-16">{children}</section>
+}
---
+++ components/sections/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section from '../Section'
+
+export default function Hero() {
+  return (
+    <>
+      <Section>
+        <div className="px-4 max-w-[1080px] m-auto">
+          <h2>About</h2>
+        </div>
+      </Section>
+    </>
+  )
+}
