8289686bc6c489dc3cb25dc3631d9135475fec0f
tyler-morales <tyler.morales.dev@gmail.com>
2022-01-29 23:22:59 -0600

    🏗️ Add section files Added a grid layout in index and section layout files

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import Hero from '../components/sections/Hero'
+
 export default function Home() {
   return (
     <>
@@ -11,6 +13,11 @@ export default function Home() {
           content="Tyler Morale's Front End Developer Portfolio"
         />
       </Head>
+
+      <div className="grid gap-12 grid-cols-1">
+        {/* Hero */}
+        <Hero />
+      </div>
     </>
   )
 }
