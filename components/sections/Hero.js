d2ca6af4cb9318d2ff3f2399b13be8e7f0b61828
tyler-morales <tyler.morales.dev@gmail.com>
2022-01-30 12:33:23 -0600

    ✨ Add Hero section text

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -6,7 +6,15 @@ export default function Hero() {
     <>
       <Section>
         <div className="px-4 max-w-[1080px] m-auto">
-          <h2>About</h2>
+          <span className="block md:text-center">👋 Hi, my name is Tyler!</span>
+          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-8xl font-bold mt-4 md:leading-normal lg:leading-[120px] md:text-center">
+            I’m a <span className="text-green">Front-end Developer</span> that
+            loves problem solving
+          </h1>
+          <p className="mt-4 md:mt-8 md:text-3xl text-lg md:text-center md:leading-relaxed md:w-[43ch] m-auto">
+            Experienced in building beautiful, interactive, and performant
+            digital web apps that are accessible for all.
+          </p>
         </div>
       </Section>
     </>
