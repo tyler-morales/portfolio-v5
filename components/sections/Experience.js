9edaa2c3daf2f539ed52de3147212a0eaed3091e
tyler-morales <tyler.morales.dev@gmail.com>
2022-01-30 21:08:33 -0600

    📱  Add responsiveness

--- components/sections/Experience.js
+++ components/sections/Experience.js
@@ -26,6 +26,18 @@ const data = [
       'Collaborated with the CEO to experiment with modern JavaScript & CSS features to construct an interactive tool to increase customers',
     ],
   },
+  {
+    id: 3,
+    name: 'Freelance',
+    role: 'Front End Developer, UI/UX Designer, and Project Manager',
+    date: 'Ongoing',
+    description: [
+      'Demonstrated expertise in translating the client’s needs into a marketing website and Netlify CMS for blog posts that resulted in faster page loads & increased user engagement',
+      'Prioritized modern image compression, accessibility features and security to achieve a Google Lighthouse score of 99/100 in performance & accessibility',
+      'Implemented evergreen technologies such as Google Maps API, Netlify forms & a modern CMS to update the client’s virtual image',
+      'Decomposed technical requirements into client understandable actions',
+    ],
+  },
 ]
 
 export default function Experience() {
@@ -38,15 +50,17 @@ export default function Experience() {
       <div className="px-4 max-w-[1080px] m-auto">
         <h2 className="text-white text-4xl font-bold mb-10">Experience</h2>
 
-        <div className="grid grid-cols-4 gap-20">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-10 md:gap-20">
           {/* Buttons */}
-          <div className="flex gap-8 flex-col justify-start">
+          <div className="flex gap-8 md:flex-col justify-start overflow-scroll md:overflow-auto">
             {data.map((job, index) => {
               return (
                 <button
                   onClick={() => setTab(job.id)}
                   key={index}
-                  className="text-lg text-white border-4 border-green rounded-lg py-2">
+                  className={`text-lg text-white border-4 border-green rounded-lg py-2 px-4 ${
+                    tab == job.id && 'bg-green'
+                  }`}>
                   {job.name}
                 </button>
               )
@@ -57,8 +71,10 @@ export default function Experience() {
             return (
               <div
                 key={index}
-                className={`col-span-3 ${tab == job.id ? 'block' : 'hidden'}`}>
-                <h3 className="text-white font-bold text-xl mb-2">
+                className={`md:col-span-3 px-4 ${
+                  tab == job.id ? 'block' : 'hidden'
+                }`}>
+                <h3 className="text-white font-bold text-2xl mb-2">
                   <span className="text-orange">{job.role}</span> @ {job.name}
                 </h3>
                 <span className="text-white mb-6 block text-lg">
